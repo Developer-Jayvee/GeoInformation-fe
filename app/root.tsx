39ea1bb9978b8c154b1d1361edc1428c02726c9a
Jayvee <jayvee.github0703@gmail.com>
2026-02-26 17:11:16 +0800

    Change tab icon path

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" type="image/png" href="../public/icons8-geology-48.png" />
+        <link rel="icon" type="image/png" href="icons8-geology-48.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
       </head>
